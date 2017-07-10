f7143e6a88762075031867965d664fdf6e593db4
chenkecheng <chenkecheng@chenkechengdeMac-mini.local>
2017-07-10 18:55:56 +0800

    commit all

---
+++ src/pages/app/app.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="contaier" :class="{'contaierb':isvariety,'contaierc':!isvariety}">
+      <lb-breakcrumb></lb-breakcrumb>
+       <lb-header @variety="variety"></lb-header>
+      <lb-body></lb-body>
+      <lb-footer></lb-footer>
+  </div>      
+</template>
+<script>
+import header from './header.vue'
+import body from './body.vue'
+import footer from './footer.vue'
+import breakcrumb from './breakcrumb.vue'
+export default{
+    name:'app',
+    data(){
+        return {
+            isvariety:false
+        }
+    },
+    components:{
+        'lb-header':header,
+        'lb-body':body,
+        'lb-footer':footer,
+        'lb-breakcrumb':breakcrumb
+    },
+    methods:{
+        variety(){
+            this.isvariety = !this.isvariety
+        }
+    }
+}
+</script>
---
+++ src/pages/app/breakcrumb.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="item1" >1</div>
+</template>
+<script>
+export default{
+    name:'breakcrumb',
+    data(){
+        return {
+            
+        }
+    },
+    
+}
+</script>
