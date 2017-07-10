87eec21b1a9ff58894e690e7dd657e51fdae0610
chenkecheng <chenkecheng@chenkechengdeMac-mini.local>
2017-07-10 18:43:10 +0800

    commit all

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-import _ from 'lodash'
 import './style/index.css'
+import './style/css/font-awesome.min.css'
+import App from './pages/app/app.vue'
+import _ from 'lodash'
+import Vue from 'vue'
+
+new Vue({
+    el: '#app',
+
+    render: h => h(App)
+})
